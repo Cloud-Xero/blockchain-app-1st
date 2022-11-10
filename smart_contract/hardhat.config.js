a9d61bc8eba4349eb308481bfc8039fd8ac5c5c4
Cloud-Xero <hiropon201511@gmail.com>
2022-11-10 22:46:08 +0900

    feat: deploy to goerli

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -2,5 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "goerli",
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_PATH,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
