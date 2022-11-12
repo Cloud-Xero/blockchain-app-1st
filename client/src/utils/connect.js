dce0ca31c9075803080362ed2ac4afbccafe4b5c
Cloud-Xero <hiropon201511@gmail.com>
2022-11-12 14:21:21 +0900

    fix: can't use process.env in client side

--- client/src/utils/connect.js
+++ client/src/utils/connect.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
 export const contractABI = abi.abi;
-export const contractAddress = process.env.CONTRACT_ADDRESS;
+export const contractAddress = "0xc1E39eDAeac50E93A07A90F79091B73C62A253Fe";
