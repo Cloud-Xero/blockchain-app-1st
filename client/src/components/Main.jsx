04239abd5369f182496f89c38682a2109d7d033d
Cloud-Xero <hiropon201511@gmail.com>
2022-11-12 14:23:45 +0900

    feat: connect TransactionContext

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TransactionContext } from "../context/TransactionContext";
 
 const Main = () => {
+  const {
+    connectWallet,
+    sendTransaction,
+    handleChange,
+    inputFormData,
+  } = useContext(TransactionContext);
+
+  const handleSubmit = (e) => {
+    const { addressTo, amount } = inputFormData;
+    e.preventDefault();
+    if (!addressTo || !amount) return;
+    sendTransaction();
+  };
+
   return (
     <div className="mainContainer">
       {/* 左側 */}
@@ -8,19 +23,29 @@ const Main = () => {
         <h1 className="title">Crypto Card</h1>
 
         <button type="button">
-          <p className="buttonText">ウォレット連携</p>
+          <p className="buttonText" onClick={connectWallet}>
+            ウォレット連携
+          </p>
         </button>
       </div>
       {/* 右側 */}
       <div className="inputContainer">
-        <input type="text" placeholder="アドレス" name="addressTo" />
+        <input
+          type="text"
+          placeholder="アドレス"
+          name="addressTo"
+          onChange={(e) => handleChange(e, "addressTo")}
+        />
         <input
           type="number"
           placeholder="通貨（ETH）"
           name="amount"
           step="0.0001"
+          onChange={(e) => handleChange(e, "amount")}
         />
-        <button type="button">送信</button>
+        <button type="button" onClick={handleSubmit}>
+          送信
+        </button>
       </div>
     </div>
   );
